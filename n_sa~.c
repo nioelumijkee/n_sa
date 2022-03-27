4a3b73d36a48e1c8511e36aebb16d3882f2ad184
Nioelumijke <harmnine9@gmail.com>
2022-03-27 03:27:46 +0300

    color and other...

--- n_sa~.c
+++ n_sa~.c
@@ -1,36 +1,20 @@
-/* n_sa - signal analysis tool
+/*----------------------------------------------------------------------------//
+ n_sa - signal analysis tool
 
-   Structure this file:
-   - init
-   - display
-   - scope
-   - spectr
-   - input methods
-   - dsp
-   - setup
++-+--------------------+-+--------------------------------+-+-------------+-+
+| |       SCOPE        | |            SPECTR              | |    PHASE    | |
++ +--------------------+ +--------------------------------+-+-------------+-+
+| |                    | |                                | |             | |
+| |                    | |                                | |             | |
+| |                    | |                                | |             | |
++-+--------------------+-+--------------------------------+-+-------------+-+
 
-
-+------+-+--------------------+-+---------------------------------+
-| MENU | |       SCOPE        | |            SPECTR               |
-+------+ +--------------------+ +---------------------------------+
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-|      | |                    | |                                 |
-+------+-+--------------------+-+---------------------------------+
-
-*/
+//----------------------------------------------------------------------------*/
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include "m_pd.h"
 
+//----------------------------------------------------------------------------//
 #define MAX_CHANNEL 16
 #define MIN_HEIGHT 100
 #define MAX_HEIGHT 1024
@@ -50,6 +34,7 @@
   if ((IN) < (MAX))      (IN) = (MAX);
 
 
+//----------------------------------------------------------------------------//
 static t_class *n_sa_class;
 
 typedef struct _n_sa_channel
@@ -65,55 +50,57 @@ typedef struct _n_sa
   t_int **v_d;      /* vector for dsp_addv */
   int amount_channel; /* body */
   t_n_sa_channel ch[MAX_CHANNEL];
-  int window; /* window open/close */
 
   /* real */
+  int window;
   int window_w_min;
   int window_h_min;
-  int window_ox;
-  int window_oy;
   int window_w;
   int window_h;
-  int menu_w;
+  int window_ox;
+  int window_oy;
   int split_w;
   int scope_w;
   int spectr_w;
-  int menu_view;
+  int phase_w;
   int scope_view;
   int spectr_view;
+  int phase_view;
 
   /* input */
-  /* int i_window_ox; */
-  /* int i_window_oy; */
-  /* int i_window_w; */
-  /* int i_window_h; */
-  /* int i_scope_w; */
-  /* int i_spectr_w; */
-  /* int i_menu_view; */
-  /* int i_scope_view; */
-  /* int i_spectr_view; */
+  int i_window_h;
+  int i_scope_w;
+  int i_spectr_w;
+  int i_scope_view;
+  int i_spectr_view;
+  int i_phase_view;
 
   int window_moved;
-  int window_size_changed;
 
-  Uint32 color_back; /* colors */
-  Uint32 color_grid;
-  Uint32 color_grid_low;
+  /* colors */
+  int i_color_split;
+  int i_color_back;
+  int i_color_grid_hi;
+  int i_color_grid_lo;
+  Uint32 color_split;
+  Uint32 color_back;
+  Uint32 color_grid_hi;
+  Uint32 color_grid_lo;
+
+  /* sdl */
   SDL_Window *win;  /* win */
   SDL_Surface *surface; /* sdl */
   int offset;
   SDL_Event event;
-  t_clock *cl;  /* clock */
+
+  /* clock */
+  t_clock *cl;
   int cl_time;
+
+  /* symbols */
   t_symbol *s_window;
   t_symbol *s_window_ox;
   t_symbol *s_window_oy;
-  t_symbol *s_window_w;
-  t_symbol *s_window_h;
-  t_symbol *s_scope_w;
-  t_symbol *s_spectr_w;
-  t_symbol *s_scope_view;
-  t_symbol *s_spectr_view;
 } t_n_sa;
 
 
@@ -121,24 +108,7 @@ typedef struct _n_sa
 
 
 //----------------------------------------------------------------------------//
-// init
-//----------------------------------------------------------------------------//
-void n_sa_init(t_n_sa *x)
-{
-  x->s_n = 64;
-  x->s_sr = 44100;
-  x->window = 0;
-  x->window_w_min = 100;
-  x->window_h_min = 200;
-  x->window_ox = 20;
-  x->window_oy = 20;
-  x->window_h = 200;
-  x->menu_w = 100;
-  x->split_w = 4;
-  x->scope_w = 200;
-  x->spectr_w = 200;
-}
-
+// various /////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------//
 void n_sa_calc_constant(t_n_sa *x)
 {
@@ -148,7 +118,7 @@ void n_sa_calc_constant(t_n_sa *x)
 //----------------------------------------------------------------------------//
 // output
 //----------------------------------------------------------------------------//
-void n_sa_output_v1(t_n_sa *x, t_symbol *s, int v)
+void n_sa_output(t_n_sa *x, t_symbol *s, int v)
 {
   t_atom a[1];
   SETFLOAT(a, (t_float)v);
@@ -156,43 +126,45 @@ void n_sa_output_v1(t_n_sa *x, t_symbol *s, int v)
 }
 
 //----------------------------------------------------------------------------//
-void n_sa_output_v2(t_n_sa *x, t_symbol *s, int v1, int v2)
+Uint32 n_sa_color(t_n_sa *x, int color)
 {
-  t_atom a[2];
-  SETFLOAT(a, (t_float)v1);
-  SETFLOAT(a+1, (t_float)v2);
-  outlet_anything(x->x_out, s, 2, a);
+  Uint32 rgb;
+  Uint8 r, g, b;
+  b = color;
+  g = color >> 8;
+  r = color >> 16;
+  rgb = SDL_MapRGB(x->surface->format, r, g, b);
+  return(rgb);
 }
 
 //----------------------------------------------------------------------------//
-// display
+void n_sa_calc_colors(t_n_sa *x)
+{
+  x->color_split = n_sa_color(x, x->i_color_split);
+  x->color_back = n_sa_color(x, x->i_color_back);
+  x->color_grid_hi = n_sa_color(x, x->i_color_grid_hi);
+  x->color_grid_lo = n_sa_color(x, x->i_color_grid_lo);
+}
+
+//----------------------------------------------------------------------------//
+// display /////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------//
 void n_sa_calc_size_window(t_n_sa *x)
 {
-  /* x->window_h = x->i_window_h; */
+  x->window_h = x->i_window_h;
+  x->scope_w = x->i_scope_w;
+  x->spectr_w = x->i_spectr_w;
+  x->scope_view = x->i_scope_view;
+  x->spectr_view = x->i_spectr_view;
+  x->phase_view = x->i_phase_view;
+
+  x->phase_w = x->window_h;
+
   x->window_w = 
-    ((x->menu_w + x->split_w) * x->menu_view) +
-    ((x->scope_w + x->split_w) * x->scope_view) + 
-    (x->spectr_w * x->spectr_view);
-  x->window_w_min = 100;
-  x->window_h_min = 100;
+    ((x->scope_w + x->split_w) * x->scope_view) +   // spectr
+    ((x->spectr_w + x->split_w) * x->spectr_view) +   // spectr
+    ((x->window_h + x->split_w) * x->phase_view);    // phase
 }
-
-//----------------------------------------------------------------------------//
-void n_sa_calc_size_areas(t_n_sa *x)
-{
-  int i;
-  float f;
-  if (x->menu_view)
-    {
-      i = x->window_w - x->menu_w;
-    }
-  float w = (float)x->scope_w / (float)x->spectr_w;
-  w = (float)i * w;
-  x->scope_w = w;
-  x->spectr_w = i - x->scope_w;
-}
-
 
 //----------------------------------------------------------------------------//
 void n_sa_reshape(t_n_sa *x)
@@ -211,30 +183,32 @@ void n_sa_redraw(t_n_sa *x)
 //----------------------------------------------------------------------------//
 void n_sa_sdl_window(t_n_sa *x)
 {
-  if (x->window)
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
     {
-      if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
-	{
-	  post("error: n_sa~: SDL_Init: %s",SDL_GetError()); 
-	}
-      x->win = SDL_CreateWindow("n_sa~",
-				x->window_ox,
-				x->window_oy,
-				x->window_w,
-				x->window_h,
-				SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-      if (x->win == NULL)
-	{
-	  post("error: n_sa~: SDL_CreateWindow: %s",SDL_GetError()); 
-	}
-      SDL_SetWindowMinimumSize(x->win, x->window_w_min, x->window_h_min);
-      clock_delay(x->cl, x->cl_time);
+      post("error: n_sa~: SDL_Init: %s",SDL_GetError()); 
     }
-  else
+  x->win = SDL_CreateWindow("n_sa~",
+			    x->window_ox,
+			    x->window_oy,
+			    x->window_w,
+			    x->window_h,
+			    SDL_WINDOW_SHOWN);
+  if (x->win == NULL)
     {
-      clock_unset(x->cl);
-      SDL_Quit();
+      post("error: n_sa~: SDL_CreateWindow: %s",SDL_GetError()); 
     }
+  SDL_SetWindowMinimumSize(x->win, x->window_w_min, x->window_h_min);
+  // clock
+  clock_delay(x->cl, x->cl_time);
+}
+
+//----------------------------------------------------------------------------//
+void n_sa_sdl_window_close(t_n_sa *x)
+{
+  // clock
+  clock_unset(x->cl);
+  // sdl
+  SDL_Quit();
 }
 
 //----------------------------------------------------------------------------//
@@ -254,7 +228,9 @@ void n_sa_sdl_get_surface(t_n_sa *x)
 //----------------------------------------------------------------------------//
 void n_sa_events(t_n_sa *x)
 {
+  // clock
   clock_delay(x->cl, x->cl_time);
+  // events
   if (x->window)
     {
       while (SDL_PollEvent(&x->event))
@@ -268,28 +244,14 @@ void n_sa_events(t_n_sa *x)
 		  post("moved");
                   x->window_ox = x->event.window.data1;
                   x->window_oy = x->event.window.data2;
-		  n_sa_output_v1(x, x->s_window_ox, x->window_ox); 
-		  n_sa_output_v1(x, x->s_window_oy, x->window_oy); 
-                  break;
-                case SDL_WINDOWEVENT_SIZE_CHANGED:
-		  post("size_changed");
-                  x->window_w = x->event.window.data1;
-                  x->window_h = x->event.window.data2;
-                  n_sa_reshape(x);
-                  n_sa_redraw(x);
-                  break;
-                case SDL_WINDOWEVENT_RESIZED:
-		  post("resized");
-		  n_sa_output_v1(x, x->s_window_w, x->window_w); 
-		  n_sa_output_v1(x, x->s_window_h, x->window_h); 
-		  n_sa_output_v1(x, x->s_scope_w, x->scope_w); 
-		  n_sa_output_v1(x, x->s_spectr_w, x->spectr_w); 
+		  n_sa_output(x, x->s_window_ox, x->window_ox); 
+		  n_sa_output(x, x->s_window_oy, x->window_oy); 
                   break;
                 case SDL_WINDOWEVENT_CLOSE:
 		  post("close");
 		  x->window = 0;
-                  n_sa_sdl_window(x);
-		  n_sa_output_v1(x, x->s_window, x->window); 
+                  n_sa_sdl_window_close(x);
+		  n_sa_output(x, x->s_window, x->window); 
                   break;
                 }
               break;
@@ -298,6 +260,9 @@ void n_sa_events(t_n_sa *x)
                 {
                 case SDLK_ESCAPE:
                   post("Escape");
+		  x->window = 0;
+                  n_sa_sdl_window_close(x);
+		  n_sa_output(x, x->s_window, x->window); 
                   break;
                 }
               break;
@@ -306,6 +271,7 @@ void n_sa_events(t_n_sa *x)
         }
     }
 
+  // after events
   if (x->window)
     {
       if (x->window_moved)
@@ -313,30 +279,29 @@ void n_sa_events(t_n_sa *x)
 	  SDL_SetWindowPosition(x->win, x->window_ox, x->window_oy);
 	  x->window_moved = 0;
 	}
-
-
-      if (x->window_size_changed)
-	{
-	  n_sa_calc_size_window(x);
-	  SDL_SetWindowSize(x->win, x->window_w, x->window_h);
-	  n_sa_reshape(x);
-	  n_sa_redraw(x);
-	  x->window_size_changed = 0;
-	}
     }
 }
 
 
 //----------------------------------------------------------------------------//
-// input methods
+// input methods ///////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------//
 static void n_sa_window(t_n_sa *x, t_floatarg f)
 {
   x->window = f;
-  n_sa_calc_size_window(x);
-  n_sa_sdl_window(x);
-  n_sa_reshape(x);
-  n_sa_redraw(x);
+  if (x->window)
+    {
+      n_sa_calc_size_window(x);
+      n_sa_sdl_window(x);
+      n_sa_sdl_get_surface(x);
+      n_sa_calc_colors(x);
+      n_sa_reshape(x);
+      n_sa_redraw(x);
+    }
+  else
+    {
+      n_sa_sdl_window_close(x);
+    }
 }
 
 //----------------------------------------------------------------------------//
@@ -357,49 +322,83 @@ static void n_sa_window_oy(t_n_sa *x, t_floatarg f)
 static void n_sa_window_h(t_n_sa *x, t_floatarg f)
 {
   CLIP_MINMAX(MIN_HEIGHT, MAX_HEIGHT, f);
-  x->window_h = f;
-  x->window_size_changed = 1;
+  x->i_window_h = f;
 }
 
 //----------------------------------------------------------------------------//
 static void n_sa_scope_w(t_n_sa *x, t_floatarg f)
 {
   CLIP_MINMAX(MIN_WIDTH_SCOPE, MAX_WIDTH_SCOPE, f);
-  x->scope_w = f;
-  x->window_size_changed = 1;
+  x->i_scope_w = f;
 }
 
 //----------------------------------------------------------------------------//
 static void n_sa_spectr_w(t_n_sa *x, t_floatarg f)
 {
   CLIP_MINMAX(MIN_WIDTH_SPECTR, MAX_WIDTH_SPECTR, f);
-  x->spectr_w = f;
-  x->window_size_changed = 1;
-}
-
-//----------------------------------------------------------------------------//
-static void n_sa_menu_view(t_n_sa *x, t_floatarg f)
-{
-  x->menu_view = f;
-  x->window_size_changed = 1;
+  x->i_spectr_w = f;
 }
 
 //----------------------------------------------------------------------------//
 static void n_sa_scope_view(t_n_sa *x, t_floatarg f)
 {
-  x->scope_view = f;
-  x->window_size_changed = 1;
+  x->i_scope_view = f;
 }
 
 //----------------------------------------------------------------------------//
 static void n_sa_spectr_view(t_n_sa *x, t_floatarg f)
 {
-  x->spectr_view = f;
-  x->window_size_changed = 1;
+  x->i_spectr_view = f;
 }
 
 //----------------------------------------------------------------------------//
-// dsp
+static void n_sa_phase_view(t_n_sa *x, t_floatarg f)
+{
+  x->i_phase_view = f;
+}
+
+//----------------------------------------------------------------------------//
+static void n_sa_color_split(t_n_sa *x, t_floatarg f)
+{
+  x->i_color_split = f * -1;
+  if (x->window)
+    {
+      x->color_split = n_sa_color(x, x->i_color_split);
+    }
+}
+
+//----------------------------------------------------------------------------//
+static void n_sa_color_back(t_n_sa *x, t_floatarg f)
+{
+  x->i_color_back = f * -1;
+  if (x->window)
+    {
+      x->color_back = n_sa_color(x, x->i_color_back);
+    }
+}
+
+//----------------------------------------------------------------------------//
+static void n_sa_color_grid_hi(t_n_sa *x, t_floatarg f)
+{
+  x->i_color_grid_hi = f * -1;
+  if (x->window)
+    {
+      x->color_grid_hi = n_sa_color(x, x->i_color_grid_hi);
+    }
+}
+
+//----------------------------------------------------------------------------//
+static void n_sa_color_grid_lo(t_n_sa *x, t_floatarg f)
+{
+  x->i_color_grid_lo = f * -1;
+  if (x->window)
+    {
+      x->color_grid_lo = n_sa_color(x, x->i_color_grid_lo);
+    }
+}
+
+//----------------------------------------------------------------------------//
+// dsp /////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------//
 t_int *n_sa_perform(t_int *w)
 {
@@ -446,7 +445,7 @@ static void n_sa_dsp(t_n_sa *x, t_signal **sp)
 }
 
 //----------------------------------------------------------------------------//
-// setup
+// setup ///////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------//
 static void *n_sa_new(t_symbol *s, int ac, t_atom *av)
 {
@@ -467,7 +466,22 @@ static void *n_sa_new(t_symbol *s, int ac, t_atom *av)
   x->x_out = outlet_new(&x->x_obj, 0);
   
   // init
-  n_sa_init(x);
+  x->s_n = 64;
+  x->s_sr = 44100;
+  x->window = 0;
+  x->window_w_min = 0;
+  x->window_h_min = 100; /* for text */
+  x->window_ox = 20;
+  x->window_oy = 20;
+  x->i_window_h = 200;
+  x->split_w = 4;
+  x->i_scope_w = 200;
+  x->i_spectr_w = 200;
+  x->i_scope_view = 0;
+  x->i_spectr_view = 0;
+  x->i_phase_view = 0;
+
+  // init
   n_sa_calc_constant(x);
 
   // mem
@@ -481,12 +495,6 @@ static void *n_sa_new(t_symbol *s, int ac, t_atom *av)
   x->s_window       = gensym("window");
   x->s_window_ox    = gensym("window_ox");
   x->s_window_oy    = gensym("window_oy");
-  x->s_window_w     = gensym("window_w");
-  x->s_window_h     = gensym("window_h");
-  x->s_scope_w      = gensym("scope_w");
-  x->s_spectr_w     = gensym("spectr_w");
-  x->s_scope_view   = gensym("scope_view");
-  x->s_spectr_view  = gensym("spectr_view");
 
   return (x);
   if (s) {};
@@ -512,7 +520,11 @@ void n_sa_tilde_setup(void)
   class_addmethod(n_sa_class,(t_method)n_sa_window_h, gensym("window_h"), A_DEFFLOAT, 0);
   class_addmethod(n_sa_class,(t_method)n_sa_scope_w, gensym("scope_w"), A_DEFFLOAT, 0);
   class_addmethod(n_sa_class,(t_method)n_sa_spectr_w, gensym("spectr_w"), A_DEFFLOAT, 0);
-  class_addmethod(n_sa_class,(t_method)n_sa_menu_view, gensym("menu_view"), A_DEFFLOAT, 0);
   class_addmethod(n_sa_class,(t_method)n_sa_scope_view, gensym("scope_view"), A_DEFFLOAT, 0);
   class_addmethod(n_sa_class,(t_method)n_sa_spectr_view, gensym("spectr_view"), A_DEFFLOAT, 0);
+  class_addmethod(n_sa_class,(t_method)n_sa_phase_view, gensym("phase_view"), A_DEFFLOAT, 0);
+  class_addmethod(n_sa_class,(t_method)n_sa_color_split, gensym("color_split"), A_DEFFLOAT, 0);
+  class_addmethod(n_sa_class,(t_method)n_sa_color_back, gensym("color_back"), A_DEFFLOAT, 0);
+  class_addmethod(n_sa_class,(t_method)n_sa_color_grid_hi, gensym("color_grid_hi"), A_DEFFLOAT, 0);
+  class_addmethod(n_sa_class,(t_method)n_sa_color_grid_lo, gensym("color_grid_lo"), A_DEFFLOAT, 0);
 }
